c766ec7023682c3a3d944283a735b24d41fc08ed
Maada Jibao <maada.jibao01@gmail.com>
2024-08-12 01:54:40 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Pmn4pRw3NVgb6QsP1rUP2y1ASm1G1o0pvgb8nXeIh86kSi0AUkgvbvFCkcRkL15dopO3sq2wpNsYnZnVQalWxAK00gMZNnlTh',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
